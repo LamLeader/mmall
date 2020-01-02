9ba4079e8b4c24342ee9af36b34d0eab9ceed103
Jack_PAN <1248414991@qq.com>
2020-01-02 23:52:12 +0800

    用户登录校验

---
+++ src/main/java/com/mmall/service/IUserService.java
@@ -0,0 +1,9 @@
+package com.mmall.service;
+
+import javax.servlet.http.HttpSession;
+
+public interface IUserService {
+
+   public  Object login(String userName, String password);
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>Hello World!Hi How are you?</h2>
 </body>
 </html>
